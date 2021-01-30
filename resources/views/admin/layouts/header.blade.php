512632a1bb5cb3b4b9d058ce7269d037cd57f50c
Sultan Atha Naufal <sultanatha9@gmail.com>
2021-01-31 04:15:41 +0700

    admin

---
+++ resources/views/admin/layouts/header.blade.php
@@ -0,0 +1,29 @@
+<div class="form-inline mr-auto">
+    <ul class="navbar-nav mr-3">
+        <li><a href="index.html#" data-toggle="sidebar" class="nav-link nav-link-lg collapse-btn"><i
+                    class="fas fa-bars"></i></a></li>
+    </ul>
+</div>
+<ul class="navbar-nav navbar-right">
+    <li class="dropdown"><a href="index.html#" data-toggle="dropdown"
+            class="nav-link dropdown-toggle nav-link-lg nav-link-user">
+            <img alt="image" src="{{asset('assets')}}/images/user.png" class="user-img-radious-style">
+            <span class="d-sm-none d-lg-inline-block"></span></a>
+        <div class="dropdown-menu dropdown-menu-right">
+            <div class="dropdown-title">Hello Jessica Hill</div>
+            <a href="profile.html" class="dropdown-item has-icon">
+                <i class="far fa-user"></i> Profile
+            </a>
+            <a href="timeline.html" class="dropdown-item has-icon">
+                <i class="fas fa-bolt"></i> Activities
+            </a>
+            <a href="index.html" class="dropdown-item has-icon">
+                <i class="fas fa-cog"></i> Settings
+            </a>
+            <div class="dropdown-divider"></div>
+            <a href="auth-login.html" class="dropdown-item has-icon text-danger">
+                <i class="fas fa-sign-out-alt"></i> Logout
+            </a>
+        </div>
+    </li>
+</ul>
---
+++ resources/views/admin/layouts/master.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+    <title>Grexa - Admin Dashboard Template</title>
+    <!-- General CSS Files -->
+    <link rel="stylesheet" href="{{asset('assets') }}/css/app.min.css">
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="{{asset('assets') }}/css/style.css">
+    <link rel="stylesheet" href="{{asset('assets') }}/css/components.css">
+    <link rel="stylesheet" href="{{asset('assets') }}/plugins/bootstrap-social/bootstrap-social.css">
+    <link rel="stylesheet" href="{{asset('assets') }}/plugins/flag-icon-css/css/flag-icon.min.css">
+    <!-- Custom style CSS -->
+    <link rel='shortcut icon' type='image/x-icon' href="{{asset('assets') }}/images/favicon.ico">
+    <!-- icons css -->
+    <link rel="stylesheet" href="{{asset('assets') }}/plugins/ionicons/css/ionicons.min.css">
+    <!-- Font css -->
+    <link rel="stylesheet" href="{{asset('assets')}}/fonts/webfonts/fa-solid-900.woff2">
+    <!-- Data Tables css -->
+    <link rel="stylesheet" href="{{asset('assets')}}/plugins/datatables/DataTables/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="{{asset('assets')}}/plugins/datatables/datatables.min.css">
+</head>
+
+<body>
+    <div class="loader"></div>
+    <div id="app">
+        <div class="main-wrapper main-wrapper-1">
+            <div class="navbar-bg"></div>
+            <nav class="navbar navbar-expand-lg main-navbar">
+                @include('admin/layouts/header')
+            </nav>
+            <div class="main-sidebar sidebar-style-2">
+                <aside id="sidebar-wrapper">
+                    @include('admin/layouts/sidebar')
+                </aside>
+            </div>
+            <!-- Main Content -->
+            @yield('content')
+            <footer class="main-footer">
+                @include('admin/layouts/footer')
+            </footer>
+        </div>
+    </div>
+    <!-- General JS Scripts -->
+    <script src="{{asset('assets')}}/js/app.min.js"></script>
+    <!-- JS Libraie assets -->
+    <script src="{{asset('assets')}}/plugins/echart/echarts.js"></script>
+
+    <script src="{{asset('assets')}}/plugins/chartjs/chart.min.js"></script>
+    <script src="{{asset('assets')}}/plugins/apexcharts/apexcharts.min.js"></script>
+    <!-- Page Specific JS File -->
+    <script src="{{asset('assets')}}/js/page/index.js"></script>
+    <!-- Template JS File -->
+    <script src="{{asset('assets')}}/js/scripts.js"></script>
+    <!-- Data Table js -->
+    <script src="{{asset('assets')}}/plugins/datatables/DataTables/js/dataTables.bootstrap4.min.js"></script>
+    <script src="{{asset('assets')}}/plugins/datatables/datatables.min.js"></script>
+</body>
+
+</html>
