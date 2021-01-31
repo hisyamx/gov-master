b8b3a1200f5fec771367946bacb92e2ca93937d8
Khusnul Hisyam <khsyaam62@gmail.com>
2021-01-31 15:30:38 +0700

    route check

--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-Auth::routes();
+// Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
@@ -66,3 +51,7 @@ Route::get('/kontak', 'MainController@kontak');
 Route::get('/ppid', 'MainController@ppid');
 //jdih
 Route::get('/jdih', 'MainController@jdih');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
