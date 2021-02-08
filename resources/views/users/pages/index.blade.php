ff3e0aa997b6158dc571e1f2c88c0403a94ca327
Khusnul Hisyam <khsyaam62@gmail.com>
2021-02-08 23:05:15 +0700

    check route

--- resources/views/users/pages/index.blade.php
+++ resources/views/users/pages/index.blade.php
@@ -90,93 +90,6 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col-lg-3 col-md-4 mt-md-0 mt-sm-5 mt-4">
-                            <div class="card">
-                                <div class="card-header p-0 position-relative">
-                                    <a href="#blog-single.html">
-                                        <img class="card-img-bottom d-block radius-image-full"
-                                            src="assets/images/image2.jpg" alt="Card image cap">
-                                    </a>
-                                </div>
-                                <div class="card-body blog-details">
-                                    <a href="#blog-single.html" class="blog-desc">2 New outfit formulas to add to your
-                                        Capsule Wardrobe
-                                    </a>
-                                    <p>Lorem ipsum dolor sit amet consectetur ipsum adipisicing elit. Quis
-                                        vitae sit.</p>
-                                    <div class="authorag align-items-center mt-3 mb-1">
-                                        <ul class="blog-meta">
-                                            <li>
-                                                <a href="author.html">Charlotte mia</a> </a>
-                                            </li>
-                                            <li class="meta-item blog-lesson">
-                                                <span class="meta-value"> July 13, 2020 </span>. <span
-                                                    class="meta-value ml-2"><span class="fa fa-clock-o"></span> 1
-                                                    min</span>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-3 col-md-4 mt-md-0 mt-sm-5 mt-4">
-                            <div class="card">
-                                <div class="card-header p-0 position-relative">
-                                    <a href="#blog-single.html">
-                                        <img class="card-img-bottom d-block radius-image-full"
-                                            src="assets/images/image2.jpg" alt="Card image cap">
-                                    </a>
-                                </div>
-                                <div class="card-body blog-details">
-                                    <a href="#blog-single.html" class="blog-desc">2 New outfit formulas to add to your
-                                        Capsule Wardrobe
-                                    </a>
-                                    <p>Lorem ipsum dolor sit amet consectetur ipsum adipisicing elit. Quis
-                                        vitae sit.</p>
-                                    <div class="authorag align-items-center mt-3 mb-1">
-                                        <ul class="blog-meta">
-                                            <li>
-                                                <a href="author.html">Charlotte mia</a> </a>
-                                            </li>
-                                            <li class="meta-item blog-lesson">
-                                                <span class="meta-value"> July 13, 2020 </span>. <span
-                                                    class="meta-value ml-2"><span class="fa fa-clock-o"></span> 1
-                                                    min</span>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-3 col-md-4 mt-md-0 mt-sm-5 mt-4">
-                            <div class="card">
-                                <div class="card-header p-0 position-relative">
-                                    <a href="#blog-single.html">
-                                        <img class="card-img-bottom d-block radius-image-full"
-                                            src="assets/images/image2.jpg" alt="Card image cap">
-                                    </a>
-                                </div>
-                                <div class="card-body blog-details">
-                                    <a href="#blog-single.html" class="blog-desc">2 New outfit formulas to add to your
-                                        Capsule Wardrobe
-                                    </a>
-                                    <p>Lorem ipsum dolor sit amet consectetur ipsum adipisicing elit. Quis
-                                        vitae sit.</p>
-                                    <div class="authorag align-items-center mt-3 mb-1">
-                                        <ul class="blog-meta">
-                                            <li>
-                                                <a href="author.html">Charlotte mia</a> </a>
-                                            </li>
-                                            <li class="meta-item blog-lesson">
-                                                <span class="meta-value"> July 13, 2020 </span>. <span
-                                                    class="meta-value ml-2"><span class="fa fa-clock-o"></span> 1
-                                                    min</span>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
@@ -221,7 +134,7 @@
     </div>
 
     {{-- berita section --}}
-    <div class="w3l-homeblock3 py-5">
+    <div class="w3l-homeblock2cstm py-1">
         <div class="container py-lg-5 py-md-4">
             <div class="w3l-error-grid py-3 text-center">
                 <div class="center-align">
@@ -229,84 +142,12 @@
                     <p>Berita seputar DPRD Provinsi Kalimantan.</p>
                 </div>
             </div>
-            <h3 class="section-title-left mb-4"> Top Pick's of this month </h3>
-            <div class="row top-pics">
-                <div class="col-lg-4 col-md-6">
-                    <div class="top-pic1">
-                        <div class="card-body blog-details">
-                            <a href="#blog-single.html" class="blog-desc">Fashion is
-                                Creating your Beauty image. The New fashion starts here
-                            </a>
-                            <div class="author align-items-center">
-                                <img src="assets/images/a1.jpg" alt="" class="img-fluid rounded-circle" />
-                                <ul class="blog-meta">
-                                    <li>
-                                        <a href="author.html">Isabella ava</a> </a>
-                                    </li>
-                                    <li class="meta-item blog-lesson">
-                                        <span class="meta-value"> July 13, 2020 </span>. <span
-                                            class="meta-value ml-2"><span class="fa fa-clock-o"></span> 1 min</span>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 mt-md-0 mt-4">
-                    <div class="top-pic2">
-                        <div class="card-body blog-details">
-                            <a href="#blog-single.html" class="blog-desc">The 5 Nonnegotiables of a Healthy Quarantine
-                                food that a Doctor Approved
-                            </a>
-                            <div class="author align-items-center">
-                                <img src="assets/images/a2.jpg" alt="" class="img-fluid rounded-circle" />
-                                <ul class="blog-meta">
-                                    <li>
-                                        <a href="author.html">Charlotte mia</a> </a>
-                                    </li>
-                                    <li class="meta-item blog-lesson">
-                                        <span class="meta-value"> July 13, 2020 </span>. <span
-                                            class="meta-value ml-2"><span class="fa fa-clock-o"></span> 1 min</span>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 mt-lg-0 mt-4">
-                    <div class="top-pic3">
-                        <div class="card-body blog-details">
-                            <a href="#blog-single.html" class="blog-desc"> Right food baked with natural ingredient, the
-                                use of best quality products
-                            </a>
-                            <div class="author align-items-center">
-                                <img src="assets/images/a3.jpg" alt="" class="img-fluid rounded-circle" />
-                                <ul class="blog-meta">
-                                    <li>
-                                        <a href="author.html">Elizabeth</a> </a>
-                                    </li>
-                                    <li class="meta-item blog-lesson">
-                                        <span class="meta-value"> July 13, 2020 </span>. <span
-                                            class="meta-value ml-2"><span class="fa fa-clock-o"></span> 1 min</span>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    {{-- berita --}}
-    <div class="w3l-homeblock2 py-5">
-        <div class="container py-lg-5 py-md-4">
             <!-- block -->
             <div class="row">
-                <div class="col-lg-8">
-                    <h3 class="section-title-left mb-4"> Editor's Pick </h3>
+                <div class="col-lg-12">
                     <div class="row">
-                        <div class="col-lg-6 col-md-6 item">
+                        {{-- section berita --}}
+                        <div class="col-lg-4 col-md-6 mt-4 item">
                             <div class="card">
                                 <div class="card-header p-0 position-relative">
                                     <a href="blog-single.html">
@@ -338,82 +179,11 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col-lg-6 col-md-6 mt-md-0 mt-sm-5 mt-4">
-                            <div class="card">
-                                <div class="card-header p-0 position-relative">
-                                    <a href="blog-single.html">
-                                        <img class="card-img-bottom d-block radius-image-full"
-                                            src="assets/images/image2.jpg" alt="Card image cap">
-                                    </a>
-                                </div>
-                                <div class="card-body blog-details">
-                                    <span class="label-blue">Fashion</span>
-                                    <a href="blog-single.html" class="blog-desc">2 New outfit formulas to add to your
-                                        Capsule Wardrobe
-                                    </a>
-                                    <p>Lorem ipsum dolor sit amet consectetur ipsum adipisicing elit. Quis
-                                        vitae sit.</p>
-                                    <div class="author align-items-center mt-3 mb-1">
-                                        <img src="assets/images/a2.jpg" alt="" class="img-fluid rounded-circle">
-                                        <ul class="blog-meta">
-                                            <li>
-                                                <a href="author.html">Charlotte mia</a>
-                                            </li>
-                                            <li class="meta-item blog-lesson">
-                                                <span class="meta-value"> July 13, 2020 </span>. <span
-                                                    class="meta-value ml-2"><span class="fa fa-clock-o"></span> 1
-                                                    min</span>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="mt-4 left-right bg-clr-white p-3">
-                        <h3 class="section-title-left align-self pl-2 mb-sm-0 mb-3">Advertise with us </h3>
-                        <a class="btn btn-style btn-primary" href="#url">Learn more</a>
-                    </div>
-                </div>
-                <div class="col-lg-4 trending mt-lg-0 mt-5">
-                    <div class="topics">
-                        <h3 class="section-title-left mb-4"> Topics</h3>
-                        <a href="beauty.html" class="topics-list">
-                            <div class="list1">
-                                <span class="fa fa-bath"></span>
-                                <h4>Beauty</h4>
-                            </div>
-                        </a>
-                        <a href="fashion.html" class="topics-list mt-3">
-                            <div class="list1">
-                                <span class="fa fa-female"></span>
-                                <h4>Fashion and style</h4>
-                            </div>
-                        </a>
-                        <a href="food.html" class="topics-list mt-3">
-                            <div class="list1">
-                                <span class="fa fa-cutlery"></span>
-                                <h4>Food and wellness</h4>
-                            </div>
-                        </a>
-                        <a href="lifestyle.html" class="topics-list mt-3">
-                            <div class="list1">
-                                <span class="fa fa-pie-chart"></span>
-                                <h4>Lifestyle</h4>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="sponsers mt-5">
-                        <h3 class="section-title-left mb-4"> Our sponsers</h3>
-                        <a href="#ad-banner"><img src="assets/images/ad.jpg" alt=""
-                                class="img-fluid radius-image-full"></a>
-                        <a href="#advertise" class="text-center d-block text-uppercase">Advertise with us </a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
     {{-- Fraksi section --}}
     <div class="w3l-homeblockfraksi py-5">
         <div class="w3l-error-grid py-3 text-center">
@@ -425,46 +195,13 @@
         {{-- isi --}}
         <div class="container py-lg-5 py-md-4">
             <div class="row top-pics">
-                <div class="col-lg-3 col-md-4">
+                <div class="col-lg-3 col-md-4 mt-4">
                     <div class="top-pic">
                         <div class="card-body blog-details">
                             <div class="author align-items-center">
                                 <img src="assets/images/a1.jpg" alt="" class="img-fluid rounded-circle" />
                             </div>
                             <a href="/fraksinasdem" class="blog-desc">Partai Nasdem
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-4">
-                    <div class="top-pic">
-                        <div class="card-body blog-details">
-                            <div class="author align-items-center">
-                                <img src="assets/images/a1.jpg" alt="" class="img-fluid rounded-circle" />
-                            </div>
-                            <a href="/fraksipdip" class="blog-desc">Partai PDI Perjuangan
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-4">
-                    <div class="top-pic">
-                        <div class="card-body blog-details">
-                            <div class="author align-items-center">
-                                <img src="assets/images/a1.jpg" alt="" class="img-fluid rounded-circle" />
-                            </div>
-                            <a href="/fraksigolkar" class="blog-desc">Partai Golongan Rakyat
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-4">
-                    <div class="top-pic">
-                        <div class="card-body blog-details">
-                            <div class="author align-items-center">
-                                <img src="assets/images/a1.jpg" alt="" class="img-fluid rounded-circle" />
-                            </div>
-                            <a href="/fraksipan" class="blog-desc">Partai PAN
                             </a>
                         </div>
                     </div>
--- routes/web.php
+++ routes/web.php
@@ -3,23 +3,99 @@
 use Illuminate\Support\Facades\Route;
 
 Auth::routes();
-//route dashboard
-Route::get('/home', 'HomeController@index')->name('home');
-// admin
-Route::group(['middleware' => ['auth']], function(){
 
- 	// main
-    Route::get('/dashboard', 'AdminController@index')->name('dashboard.index');
-    // Route::get('/dashboard/iconbanner', 'MainController@index')->name('dashboard.iconbanner');
-    
-	//logout
-	Route::get('/logout', 'AuthManageController@logoutProcess');
+/// Admin Route
+Route::group(['prefix' => 'admin', 'middleware' => 'admin'], function () {
+    // Show Welcome
+    Route::get('', 'AdminController@index')->name('dashboard.index');
+    Route::get('welcome', 'AdminController@index')->name('dashboard.index');
+    Route::get('statistik', 'AdminController@showStatistic');
+    Route::get('buat-artikel', 'AdminController@showBuatArtikel');
+    Route::post('buat-artikel', 'AdminController@buatArtikel');
+    // Show Kelola beranda
+    Route::get('kelola-beranda', 'AdminController@showKelolaBeranda');
+    Route::get('terbit-beranda/{id}', 'AdminController@terbitBeranda');
+    Route::get('tunda-beranda/{id}', 'AdminController@tundaBeranda');
+    Route::get('edit-beranda/{id}', 'AdminController@showEditBeranda');
+    Route::post('edit-beranda/{id}', 'AdminController@editBeranda');
+    Route::get('hapus-beranda/{id}', 'AdminController@deleteBeranda');
+    Route::get('draft-beranda', 'AdminController@showDraftBeranda');
+    // Show Kelola profil
+    Route::get('kelola-profil', 'ProfilController@showKelola');
+    Route::get('terbit-profil/{id}', 'ProfilController@terbit');
+    Route::get('tunda-profil/{id}', 'ProfilController@tunda');
+    Route::get('edit-profil/{id}', 'ProfilController@showEdit');
+    Route::post('edit-profil/{id}', 'ProfilController@edit');
+    Route::get('hapus-profil/{id}', 'ProfilController@delete');
+    Route::get('draft-profil', 'ProfilController@showDraft');
+    // Show Kelola akd
+    Route::get('kelola-akd', 'AkdController@showKelola');
+    Route::get('terbit-akd/{id}', 'AkdController@terbit');
+    Route::get('tunda-akd/{id}', 'AkdController@tunda');
+    Route::get('edit-akd/{id}', 'AkdController@showEdit');
+    Route::post('edit-akd/{id}', 'AkdController@edit');
+    Route::get('hapus-akd/{id}', 'AkdController@delete');
+    Route::get('draft-akd', 'AkdController@showDraft');
+    // Show Kelola fraksi
+    Route::get('kelola-fraksi', 'FraksiController@showKelola');
+    Route::get('edit-fraksi/{id}', 'FraksiController@showEdit');
+    Route::post('edit-fraksi/{id}', 'FraksiController@edit');
+    Route::get('hapus-fraksi/{id}', 'FraksiController@delete');
+    Route::get('kelola-angota', 'FraksiController@showKelolaAnggota');
+    Route::get('edit-angota/{id}', 'FraksiController@showEditAnggota');
+    Route::post('edit-angota/{id}', 'FraksiController@editAnggota');
+    Route::get('hapus-angota/{id}', 'FraksiController@deleteAnggota');
+    // Show Kelola informasi
+    Route::get('kelola-informasi', 'InformasiController@showKelola');
+    Route::get('terbit-informasi/{id}', 'InformasiController@terbit');
+    Route::get('tunda-informasi/{id}', 'InformasiController@tunda');
+    Route::get('edit-informasi/{id}', 'InformasiController@showEdit');
+    Route::post('edit-informasi/{id}', 'InformasiController@edit');
+    Route::get('hapus-informasi/{id}', 'InformasiController@delete');
+    Route::get('draft-informasi', 'InformasiController@showDraft');
+    // Show Kelola sekretariat
+    Route::get('kelola-sekretariat', 'SekretariatController@showKelola');
+    Route::get('terbit-sekretariat/{id}', 'SekretariatController@terbit');
+    Route::get('tunda-sekretariat/{id}', 'SekretariatController@tunda');
+    Route::get('edit-sekretariat/{id}', 'SekretariatController@showEdit');
+    Route::post('edit-sekretariat/{id}', 'SekretariatController@edit');
+    Route::get('hapus-sekretariat/{id}', 'SekretariatController@delete');
+    Route::get('draft-sekretariat', 'SekretariatController@showDraft');
+    // Show Kelola publikasi
+    Route::get('kelola-gallery', 'ProfilController@showKelola');
+    Route::get('kelola-gallery', 'ProfilController@showKelola');
+    Route::get('kelola-gallery', 'ProfilController@showKelola');
+    Route::get('terbit-profil/{id}', 'ProfilController@terbit');
+    Route::get('tunda-profil/{id}', 'ProfilController@tunda');
+    Route::get('edit-profil/{id}', 'ProfilController@showEdit');
+    Route::post('edit-profil/{id}', 'ProfilController@edit');
+    Route::get('hapus-profil/{id}', 'ProfilController@delete');
+    Route::get('draft-profil', 'ProfilController@showDraft');
+    // Show Kelola kontak
+    Route::get('kelola-kontak', 'KontakController@showKelola');
+    Route::get('edit-kontak/{id}', 'KontakController@showEdit');
+    Route::post('edit-kontak/{id}', 'KontakController@edit');
+    Route::get('hapus-kontak/{id}', 'KontakController@delete');
+    // Show Kelola User
+    Route::get('kelola-user', 'AdminController@showKelolaUser');
+    Route::get('edit-user/{id}', 'AdminController@showEditUser');
+    Route::put('edit-user/{id}', 'AdminController@editUser');
+    Route::get('delete-user/{id}', 'AdminController@deleteUser');
+    Route::get('buat-user', 'AdminController@showBuatUser');
+    Route::post('buat-user', 'AdminController@buatUser');
 });
 
+/// Author Route
+Route::group(['prefix' => 'author', 'middleware' => 'author'], function () {
+    //
+    Route::get('', 'AuthorController@showWelcome');
+    Route::get('welcome', 'AuthorController@showWelcome');
+    Route::get('profil', 'AuthorController@showProfil');
+    Route::get('buat-artikel', 'AuthorController@showBuatArtikel');
+    Route::get('kelola-artikel', 'AuthorController@showKelolaArtikel');
+});
 
-Route::get('/home', 'HomeController@index')->name('home');
-
-//route user
+///User Route
 Route::get('/', 'MainController@beranda');
 //profil
 Route::get('/tentangdprd', 'ProfilController@tentangdprd');
@@ -34,7 +110,7 @@ Route::get('/agenda/sekretariat', 'AgendaController@agendasekretariat');
 //berita
 Route::get('/berita', 'BeritaController@berita');
 Route::get('/berita/pressrelease', 'BeritaController@pressrelease');
-Route::get('/berita/{detailberita}', 'BeritaController@detailberita');
+Route::get('/berita/{id}', 'BeritaController@detailberita');
 //akd
 Route::get('/akd/komisi', 'AkdController@komisi');
 Route::get('/akd/pimpinandprd', 'AkdController@pimpinandprd');
@@ -45,7 +121,7 @@ Route::get('/akd/badanperda', 'AkdController@badanperda');
 //fraksi
 Route::get('/fraksi', 'FraksiController@fraksi');
 Route::get('/fraksi/{namafraksi}', 'FraksiController@detailfraksi');
-Route::get('/fraksi/{namaanggotafraksi}', 'FraksiController@detailanggotafraksi');
+Route::get('/fraksi/{namafraksi}/{id}', 'FraksiController@detailanggotafraksi');
 //sekreatariat
 Route::get('/sekretariat/rencanalaporan', 'SekretariatController@rencanalaporan');
 Route::get('/sekretariat/tugasfungsi', 'SekretariatController@tugasfungsi');
